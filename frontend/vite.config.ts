13d515073b8c599c8e530026292f23dc9701f76e
Anson Chung <58066418+anscg@users.noreply.github.com>
2026-03-18 03:56:31 +0800

    allow all host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     host: true,
     port: 5173,
     watch: {
       usePolling: true,
-    }
-  }
-})
+    },
+  },
+});
